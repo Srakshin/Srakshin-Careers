0c78e519e1390e375feca93e144f77992653f335
srakshin <bsrakshin@gmail.com>
2025-03-23 10:36:46 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+Jobs = [
+    {
+      'Id':1,
+      'Title':'Frontend Engineering',
+      'Location':'Hyderabad',
+      'Salary': 100000
+    },
+    {
+      'Id':2,
+      'Title':'Backend Engineering',
+      'Location':'Maharastra',
+      'Salary': 200000
+    },
+    {
+      'Id':3,
+      'Title':'Editor',
+      'Location':'Gachibowli',
+      'Salary': 300000
+    }
+]
+
+@app.route("/")
+def hello_world():
+    return render_template('Home.html',jobs=Jobs)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
